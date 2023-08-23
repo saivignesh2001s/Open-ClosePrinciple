dcd8c9a6db643b6f2c437c0c47e0963fe0d57747
saivignesh.p <saivignesh.p@LTPCHE032529213>
2023-08-23 15:13:59 +0530

    Add project files.

---
+++ Course.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_ClosePrinciple
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    internal abstract class Course
+    {
+        public int courseid { get; set; }
+        public string coursename { get; set; }
+        public abstract void subscribe(Student std,Course cs);
+    }
+    internal class onlinecourse : Course
+    {
+       
+        public override void subscribe(Student std,Course cs)
+        {
+            Console.WriteLine($"{std.Name} registered the online course {cs.coursename}");
+        }
+    }
+    internal class offlinecourse : Course {
+        public override void subscribe(Student std, Course cs)
+        {
+            Console.WriteLine($"{std.Name} registered the offline course {cs.coursename}");
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Open_ClosePrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Id = 1;
+            student.Name = "Ranga";
+            Course course = new onlinecourse();
+            course.courseid = 1;
+            course.coursename = "C++";
+            course.subscribe(student, course);
+            Course course1 = new offlinecourse();
+            course1.courseid = 2;
+            course1.coursename = "C#";
+            course1.subscribe(student, course1);
+        }
+    }
+}
